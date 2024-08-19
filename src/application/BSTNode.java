299a07650e23a57e93696af116bb41dc942089b4
Shoyeb45 <shoyebff45@gmail.com>
2024-08-20 00:13:00 +0530

    Succefully implemented algorithm for balancing BST visually, and also added comments

--- src/application/BSTNode.java
+++ src/application/BSTNode.java
@@ -1,44 +1,99 @@
 package application;
 
 import javafx.geometry.Bounds;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 
 public class BSTNode {
-	public static final double RADIUS = 20;
-	public int value;
-	public BSTNode left;
-	public BSTNode right;
-	public Circle circle;
-	public Text text;
 	
+	/*
+	 * Constants
+	 * */
+	public static final double RADIUS = 20; // Radius of node
+	
+	/*
+	 * Attributes of BSTNode class
+	 * */
+	public int value; 		  // To hold the data in node
+	public BSTNode left;      // To hold the left child of node
+	public BSTNode right;     // To hold the right child of node
+	public Circle circle;     // Circle for enclosing the value inside it
+	public double leftWidth;  // To adjust according to number of nodes in left sub-tree
+	public double rightWidth; // To adjust according to number of nodes in left sub-tree
+	public Text text; 		  // Text object for enclosing the value inside the circle
+	public Line rightEdge;    // Line to show the pointer between node and left sub-tree
+	public Line leftEdge;     // Line to show the pointer between node and right sub-tree
+	
+	
+	/*
+	 * Constructor to initialize a BSTNode
+	 * */
 	BSTNode(int data, double centerX, double centerY) {
-		value = data;
-		left = right = null;
-		circle = new Circle(RADIUS, Color.LIGHTBLUE);
-		text = new Text(String.valueOf(data));
 		
-		// Setting center of circle
-		circle.setCenterX(centerX);
-		circle.setCenterY(centerY);
-		circle.setStroke(Color.BLACK);
-		circle.setStrokeWidth(2);
-		Bounds bounds = text.getBoundsInLocal();
-        double textWidth = bounds.getWidth();
-        double textHeight = bounds.getHeight();
-
-        text.setX(centerX - textWidth / 2);
-        text.setY(centerY + textHeight / 4);
+		value = data;						   // Assigning value to given value(data)
+		left = right = null;				   // Assigning left and right pointer to null
+		circle = new Circle(RADIUS);		   // Assigning new Circle object to circle with radius RADIUS
+		text = new Text(String.valueOf(data)); // Assigning new Text object to text with data value
+		leftWidth = rightWidth = 0;			   // For new node the left width and right width will be 0
+		rightEdge = new Line();				   // Assigning new Line object to rightEdge(i.e., edge connecting left and current node)
+		leftEdge = new Line();                 // Assigning new Line object to leftEdge(i.e., edge connecting right and current node)
+		
+		
+		// circle :-
+		circle.setCenterX(centerX);           // Setting x-coordinate of center
+		circle.setCenterY(centerY);			  // Setting y-coordinate of center	
+		circle.setStroke(Color.BLACK);        // Setting stroke color to black of circle
+		circle.setStrokeWidth(2);             // Setting stroke width of circle
+		circle.setFill(Color.LIGHTBLUE);      // Setting background color of circle
+		
+		// text :- 
+		Bounds bounds = text.getBoundsInLocal();  // Getting bound parameters for setting the text in center of circle
+        double textWidth = bounds.getWidth();     // Getting width of text
+        double textHeight = bounds.getHeight();   // Getting height of text
+        text.setX(centerX - textWidth / 2);		  // Setting x-coordinate of text in center of circle	
+        text.setY(centerY + textHeight / 4);	  // Setting y-coordinate of text in center of circle
+        
 	}
 	
+	/*
+	 * Method for updating positions with new coordinates-(x, y)
+	 * */
 	public void updatePositions(double x, double y) {
-		this.circle.setCenterX(x);
-		this.circle.setCenterX(y);
+		this.circle.setCenterX(x);   // Set new x-coordinate
+		this.circle.setCenterY(y);   // Set new y-coordinate
+		
+		// Set text
 		Bounds bounds = this.text.getBoundsInLocal();
 		double textW = bounds.getWidth(), textH = bounds.getHeight();
 		
 		text.setX(x - textW / 2);
-		text.setY(y - textH);
+		text.setY(y + textH / 4);
+		
+		
+		// Set the coordinates of lines, the starting and ending position will be same to show the growing animation of line
+		// For left edge
+		leftEdge.setStartX(x - BinarySearchTree.DIST);
+        leftEdge.setStartY(y + BinarySearchTree.K);
+        leftEdge.setEndX(x - BinarySearchTree.DIST);
+        leftEdge.setEndY(y + BinarySearchTree.K);
+        
+        // For right edge
+        rightEdge.setStartX(x + BinarySearchTree.DIST);
+        rightEdge.setStartY(y + BinarySearchTree.K);
+        rightEdge.setEndX(x + BinarySearchTree.DIST);
+        rightEdge.setEndY(y + BinarySearchTree.K);
+        leftEdge.setStrokeWidth(0);
+        rightEdge.setStrokeWidth(0);
+	}
+	
+	public static void showLine(Line line) {
+		line.setStrokeWidth(1);
+	}
+	
+	public void addLineToPane(Pane pane) {
+		pane.getChildren().addAll(this.rightEdge, this.leftEdge);
 	}
 }
--- src/application/BinarySearchTree.java
+++ src/application/BinarySearchTree.java
@@ -1,30 +1,38 @@
 package application;
 
 import javafx.util.Duration;
-import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
-import javafx.animation.PauseTransition;
 import javafx.animation.ScaleTransition;
-import javafx.animation.SequentialTransition;
 import javafx.animation.Timeline;
-import javafx.geometry.Bounds;
 import javafx.scene.control.Alert;
 import javafx.scene.effect.Glow;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
-import javafx.scene.text.Text;
 
 public class BinarySearchTree {
 	
-	private static final int START_Y = 50;
-    private static final int DELTA_X = 40; // Horizontal spacing
-    private static final int DELTA_Y = 45; // Vertical spacing between levels
-	private static final double K = 10; // Distance of chord from center, to calculate the start of edges
-	private BSTNode root; 
-	private Pane pane;
+	/*
+	 * Constant Values
+	 * */
+	public final static int START_Y = 50; // Initial coordinate of binary search tree node in y-axis 
+    public final static int DELTA_X = 50; // Horizontal spacing
+    public final static int DELTA_Y = 50; // Vertical spacing between levels
+	public final static double K = 13;    // Distance of chord from center, to calculate the start of edges
+	public final static double DIST = Math.sqrt(Math.pow(BSTNode.RADIUS, 2) - Math.pow(K, 2));  // Distance of line from the edge point starting to perpendicular bisector
 	
+	
+	/*
+	 * Attributes of Binary Search Tree Class
+	 * */
+	public double startX;
+	public BSTNode root; 
+	public Pane pane;
+	
+	/*
+	 * Constructor sets root to null and initializes pane
+	 * */
 	BinarySearchTree(Pane newPane) {
 		pane = newPane;
 		root = null;
@@ -34,14 +42,19 @@ public class BinarySearchTree {
 	 * Method to insert the value in binary search tree
 	 * */
 	public void insert(int val) {
+		
+		// When binary search tree is empty
 		if(root == null) { 
-			BSTNode newNode = new BSTNode(val, pane.getWidth() / 2,  START_Y); // insert at the center of the pane
+			this.startX = pane.getWidth() / 2; // At middle of pane
+			BSTNode newNode = new BSTNode(val, startX,  START_Y); // insert at the center of the pane
 			root = newNode;
 			pane.getChildren().addAll(newNode.circle, newNode.text); // Add node to pane
+			root.addLineToPane(pane); // Add both the left and right lines to pane
 			return; 
 		}
 		if(!this.contains(this.root, val)) {
 			this.insertUtil(this.root, val, root.circle.getCenterX(), root.circle.getCenterY(), false); // Helper method to insert the new value
+			resizeTree(); // For visually balancing the binary search Tree
 		}
 	}
 	
@@ -92,56 +105,66 @@ public class BinarySearchTree {
 	
 	/*
 	 * Utility Method for inserting new node in given position
-	 * 
 	 * */
 	private BSTNode insertUtil(BSTNode root, int val, double centerX, double centerY, boolean isLeft) {
+		
 		if(root == null) {
 			double newCenterX, newCenterY = centerY + DELTA_Y;  // Center of new node
-			
 			BSTNode newNode;
-			double dist = Math.sqrt(Math.pow(BSTNode.RADIUS, 2) - Math.pow(K, 2)); // Distance of line from the edge point starting to perpendicular bisector
-			
-			
-			double lineStartX = 0, lineStartY = centerY + K; // Starting positions of line
 			
 			if(isLeft) {
 				newCenterX = centerX - DELTA_X; // center of new node
-				newNode = new BSTNode(val, newCenterX, newCenterY); // create node at that center
-				lineStartX = centerX - dist; // starting point of line
 			} else {
 				newCenterX = centerX + DELTA_X;
-				newNode = new BSTNode(val, newCenterX, newCenterY);
-				lineStartX = centerX + dist; // starting point of line
 			}
 			
-			double lineEndX = newCenterX, lineEndY = newCenterY - 20; // End points of line 
-			
-			Line line = new Line(lineStartX, lineStartY, lineStartX, lineStartY);	// Drawing line from start(x,y) -> start(x,y), later at animation the end(x,y) is paassed
-			pane.getChildren().addAll(newNode.circle, newNode.text, line); // add to the pane
-			
-			nodeAnimationOfInsertion(newNode, line, lineEndX, lineEndY); // Animation
+			newNode = new BSTNode(val, newCenterX, newCenterY); // creating new node
+			pane.getChildren().addAll(newNode.circle, newNode.text); // add to the pane
+			newNode.addLineToPane(pane);
+			nodeAppearance(newNode);
 			return newNode;
 		}
 		
 		if(root.value < val) {
-			root.right = insertUtil(root.right, val, root.circle.getCenterX(), root.circle.getCenterY(), false);  // visit right Subtree
+			// visit right Subtree
+			root.right = insertUtil(root.right, val, root.circle.getCenterX(), root.circle.getCenterY(), false);  
 		} else {
-			root.left = insertUtil(root.left, val, root.circle.getCenterX(), root.circle.getCenterY(), true); // Visit left subtree
+			// Visit left subtree
+			root.left = insertUtil(root.left, val, root.circle.getCenterX(), root.circle.getCenterY(), true); 
 		}
 		return root;
 	}
 	
 	/*
-	 * Method for node animation at insertion
+	 * Method for animation of node appearance at insertion
 	 * */
-	private void nodeAnimationOfInsertion(BSTNode newNode, Line line , double endX, double endY) {
+	private void nodeAppearance(BSTNode newNode) {
 		ScaleTransition nodeAppearance = new ScaleTransition(Duration.seconds(0.5), newNode.circle);
         nodeAppearance.setFromX(0.1);
         nodeAppearance.setFromX(0.1);
         nodeAppearance.setToX(1);
         nodeAppearance.setToY(1);
         
-        Timeline timeline = new Timeline();
+//        Timeline timeline = new Timeline();
+//
+//	    KeyFrame keyFrame = new KeyFrame(
+//	            Duration.seconds(1), // Animation duration
+//	            new KeyValue(line.endXProperty(), endX),
+//	            new KeyValue(line.endYProperty(), endY)
+//	    );
+//	    
+//	    timeline.getKeyFrames().add(keyFrame);
+//	    SequentialTransition sq = new SequentialTransition();
+//	    sq.getChildren().addAll(nodeAppearance, timeline);
+//	    sq.play();
+        nodeAppearance.play();
+	}
+	
+	/*
+	 * Method for animation of line growing
+	 * */
+	private void lineGrowing(Line line, double endX, double endY) {
+		Timeline timeline = new Timeline();
 
 	    KeyFrame keyFrame = new KeyFrame(
 	            Duration.seconds(1), // Animation duration
@@ -150,12 +173,68 @@ public class BinarySearchTree {
 	    );
 	    
 	    timeline.getKeyFrames().add(keyFrame);
-	    SequentialTransition sq = new SequentialTransition();
-	    sq.getChildren().addAll(nodeAppearance, timeline);
-	    sq.play();
+	    timeline.play();
 	}
 	
+	private void resizeTree() {
+		
+		double startingPoint = this.startX;
+		this.resizeWidths(root);  // Update the left and right width of every node
+		
+		if(root != null) {
+			// Update starting point
+			if(this.root.leftWidth > startingPoint) {
+				startingPoint = this.root.leftWidth;
+			} else if(this.root.rightWidth > startingPoint) {
+				startingPoint = Math.max(this.root.leftWidth, 2 * startingPoint - this.root.rightWidth);
+			}
+			
+			// Assign new positions
+			root.updatePositions(startingPoint, START_Y);
+			
+			this.setNewPositions(root.left, startingPoint, START_Y + DELTA_Y, -1, startingPoint - DIST, START_Y + K, root.leftEdge);				
+			this.setNewPositions(root.right, startingPoint, START_Y + DELTA_Y, 1, startingPoint + DIST, START_Y + K, root.rightEdge);
+		}
+	}
+
+	private void setNewPositions(BSTNode node, double xPosition, double yPosition, int side, double lineStartX, double lineStartY, Line line) {
+	    if (node == null) {
+	        return;
+	    }
+	    if (side == -1) {
+	        // Position the node based on the left width
+	        xPosition = xPosition - node.rightWidth;
+	    } else if (side == 1) {
+	        // Position the node based on the right width
+	        xPosition = xPosition + node.leftWidth;
+	    }
+	    
+	    // Update the center of circle based on new positions
+	    node.updatePositions(xPosition, yPosition);
+	    // make lines visible
+	    BSTNode.showLine(line);
+	    // Show animation for growing line from start -> end	    
+	    lineGrowing(line, xPosition, yPosition - BSTNode.RADIUS);
+	    
+
+	    // Recursively set positions for left and right children
+	    setNewPositions(node.left, xPosition, yPosition + DELTA_Y, -1, xPosition - DIST, yPosition + K, node.leftEdge);
+	    setNewPositions(node.right, xPosition, yPosition + DELTA_Y, 1, xPosition + DIST, yPosition + K, node.rightEdge);
+	}
 	
+	private double resizeWidths(BSTNode node) {
+		if (node == null) {
+			return 0;
+		}
+		
+		// Calculate widths for left and right subtrees
+		node.leftWidth = Math.max(resizeWidths(node.left), DELTA_X / 2);
+		node.rightWidth = Math.max(resizeWidths(node.right), DELTA_X / 2);
+		
+		// Return the combined width
+		return node.leftWidth + node.rightWidth;
+	}
+
 	
 	
 	/*
